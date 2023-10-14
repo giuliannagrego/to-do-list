e81bec3076d374a2b58365972f998d7115d54d2a
giuliannagrego <giuliannagrego@yahoo.com.br>
2023-10-13 22:17:56 -0300

    att desenvolvimento em aula

--- src/main/java/giuliannagrego/todolist/task/entities/TaskModel.java
+++ src/main/java/giuliannagrego/todolist/task/entities/TaskModel.java
@@ -1,27 +1,32 @@
-package giuliannagrego.todolist.user.entities;
+package giuliannagrego.todolist.task.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
+
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
-@Entity(name ="tb_users")
-public class UserModel {
+@Entity(name = "tb_tasks")
+public class TaskModel {
 
     @Id
     @GeneratedValue(generator = "UUID")
     private UUID id;
+    private String description;
 
-    @Column(unique = true)
-    private String username;
-    private String name;
-    private String password;
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    private UUID idUser;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
